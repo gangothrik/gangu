0d96b791ca7b1ac2887a1418476dfe8e8f425bdd
gangothrik <Gangothrik03@gmail.com>
2024-12-02 10:36:19 +0530

    Add files via upload

---
+++ SIZE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+clrscr();
+printf("\n size of short int=%d",sizeof(short int));
+printf("\n size of int=%d",sizeof(int));
+printf("\n  size of long int=%d",sizeof(long int));
+printf("\n size of unsigned int =%d",sizeof(unsigned int));
+printf("\n size of double =%d",sizeof(double));
+getch();
+}
